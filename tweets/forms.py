efa8784a05d4e4a8dc182873f043bc0e49dcf8bb
ghaiyur <ghaiyur@techis.io>
2021-03-26 14:47:26 +0530

    Cleanup post fail

--- tweets/forms.py
+++ tweets/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 from django.utils.html import strip_tags
 from .models import Tweet
+import cloudinary
+from cloudinary.models import CloudinaryField
 
 
 class TweetForm(forms.ModelForm):
@@ -10,7 +12,6 @@ class TweetForm(forms.ModelForm):
                                     'placeholder': 'Tweet',
                                     'class': 'form-control'
                                 }))
-
     class Meta:
         model = Tweet
         exclude = ('user', )

8ab60765257fd7b947a4fbcf5be8b09c035834ba
ghaiyur <ghaiyur@techis.io>
2021-04-06 11:46:03 +0530

    URLed like , and added to svg as href

--- config/urls.py
+++ config/urls.py
@@ -2,15 +2,20 @@ from django.contrib import admin
 from django.urls import path
 from tweets.views import feed , TweetDelete
 from users.views import frontpage, signout, profile
-from users.views import following, followers, follow, stopfollow
+from users.views import following, followers, follow, stopfollow , tweetLikeAdd, tweetLikeSubtract
 
 
 urlpatterns = [
+    ### Pages
     path('admin/', admin.site.urls),
     path('', frontpage, name='frontpage'),
     path('feed/', feed, name='feed'),
     path('signout/', signout, name='signout'),
+    ### Tweet Activities
     path('TweetDelete/<int:tweet_id>/', TweetDelete),
+    path('tweetLikeAdd/<int:tweet_id>/', tweetLikeAdd),
+    path('tweetLikeSubtract/<int:tweet_id>/', tweetLikeSubtract),
+    ### User Activities
     path('<str:username>/', profile, name='profile'),
     path('<str:username>/following', following, name='following'),
     path('<str:username>/followers', followers, name='followers'),
